b42333412cd9d432f013abfa4429e245a1822d45
pierre <pierre.garcia@u-psud.fr>
2019-11-13 15:24:52 +0100

    architectureChoice

--- mesh.hpp
+++ mesh.hpp
@@ -1,6 +1,17 @@
 #ifndef MESH
 #define MESH
-#include lib.hpp
+#include <glm/gtc/type_ptr.hpp>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+
+#include <string.h>
+#include <vector>
 
 struct Vertex {
     glm::vec3 Position;
@@ -17,12 +28,12 @@ struct Texture {
 class Mesh {
     public:
         /*  Mesh Data  */
-        vector<Vertex> vertices;
-        vector<unsigned int> indices;  
-        vector<Texture> textures;
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;  
+        std::vector<Texture> textures;
         /*  Functions  */
-        Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
-        void Draw(Shader shader);
+        Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+        void Load();
     private:
         /*  Render data  */
         unsigned int VAO, VBO, EBO;
---
+++ quickAndDirtyIteration.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
+#include <GL/gl.h>
+#endif
+using namespace glm;
